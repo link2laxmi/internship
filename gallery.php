1571b697baf723a842cbb63054dcc77964b68adc
laxmi regmi <laxmiregmi999@gmail.com>
2019-09-22 10:42:18 -0700

    modified galler.php code

--- gallery.php
+++ gallery.php
@@ -6,12 +6,13 @@
 		 <link rel="stylesheet" href="main.css" />
 	</head>
 
- <body>
- 	<a href="gallery.php">gallery</a>
+<body>
+ 	
  	<div class="container">
- 		 <header>
+ 		<header>
  		 	<div class="logo">
- 		 		<img src="img/sochware.gif"></div>
+ 		 		<img src="img/sochware.gif">
+ 		 	</div>
  		 		<nav>
  		 			<ul>
  		 				<li><a href="#">home</a></li>
@@ -20,50 +21,61 @@
  		 				<li><a href="#">career</a></li>
  		 				<li><a href="#">contact</a></li>
  		 			</ul>
- 		 		</nav>
- 		 	</header>
- 		 	<div class="box">
- 		 		<a href="image.php">
-					<img src="img/project.jpeg">
-				</a>
- 		 		</div>
- 		 		<div class="image">
- 		 			<a href="image.php"><h1 style="text-align:center; font-size:35px;"><h1></a></div>
- 		 		</div>
+ 		 		</nav><
+ 		</header>
+ 		<div class="box">
+ 		    <a href="image.php">
+			    <img src="img/project.jpeg">
+			</a>
+ 		</div>
+ 		<div class="image">
+ 		 	<a href="image.php"><h1 style="text-align:center; font-size:35px;"></h1>>
+ 		 	</a>
  		 	
- 		 	<div class="content">
- 		 		<div class="left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ipsam<br> quia dolores ad pariatur fuga quam a placeat consequatur, soluta eaque <br>minus praesentium, porro, beatae quis quisquam corporis illum ipsum.</div>
- 		 		<div class="right">
- 		 			<table border="1"; cellspacing=10px;cellpadding=0; width:100%;>
- 		 				<tr>
- 		 					<td>id</td>
- 		 					<td>name</td>
- 		 					<td>email</td>
- 		 				</tr>
+ 		</div>
+   </div>
+ 		 	
+   <div class="content">
+ 	    <div class="left">
+ 		    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ipsam
+ 		    <br> 
+ 		    quia dolores ad pariatur fuga quam a placeat consequatur, soluta eaque 
+ 		    <br>
+ 		    minus praesentium, porro, beatae quis quisquam corporis illum ipsum
+ 		</div>
+ 		<div class="right">
+ 		    <table border="1"; cellspacing=10px;cellpadding=0; width:100%;>
+ 		 	    <tr>
+					<td>id</td>
+					<td>name</td>
+					<td>email</td>
+				</tr>
 
- 		 				<tr>
- 		 					<td>id</td>
- 		 					<td>name</td>
- 		 					<td>email</td>
- 		 				</tr>
+				<tr>
+					<td>id</td>
+					<td>name</td>
+					<td>email</td>
+				</tr>
 
- 		 				<tr>
- 		 					<td>id</td>
- 		 					<td>name</td>
- 		 					<td>email</td>
- 		 				</tr>
+				<tr>
+					<td>id</td>
+					<td>name</td>
+					<td>email</td>
+				</tr>
 
- 		 				<tr>
- 		 					<td>id</td>
- 		 					<td>name</td>
- 		 					<td>email</td>
- 		 				</tr>
- 		 			</table>
- 		 			<div class="clc"></div>
-
- 		 		</div>
- 		 		<div class="clc"></div>
- 		 		<footer></footer>
- 		 	</div>
- 		 </body>
- 		 </html>
+				<tr>
+					<td>id</td>
+					<td>name</td>
+					<td>email</td>
+				</tr>
+		    </table>
+ 		    <div class="clc">
+ 		    </div>
+ 	    </div>
+ 		<div class="clc">
+ 	    </div>
+ 	    <footer>
+ 	    </footer>
+ 	</div>
+ </body>
+</html>
